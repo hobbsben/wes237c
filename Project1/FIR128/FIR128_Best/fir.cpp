933464e5a71a8e11fd6f0764c7867b55baa7dd5f
Frank Chang <fychang@ucsd.edu>
2018-10-11 23:43:58 -0700

    WES237 - Project1 submission

---
+++ Project1/FIR128/FIR128_Best/fir.cpp
@@ -0,0 +1,55 @@
+/*
+	Filename: fir.cpp
+		FIR lab wirtten for WES/CSE237C class at UCSD.
+		Match filter
+	INPUT:
+		x: signal (chirp)
+
+	OUTPUT:
+		y: filtered output
+
+*/
+#include <string.h>
+#include <math.h>
+#include "fir.h"
+
+void fir (data_t *y, data_t x)
+{
+
+	coef_t c[N] = {10, 11, 11, 8, 3, -3, -8, -11, -11, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -11, -11, -8, -3, 3, 8, 11, 11, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 8, 3, -3, -8, -11, -11, -10, -10, -10, -10, -10, -10, -10, -10, -11, -11, -8, -3, 3, 8, 11, 11, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 8, 3, -3, -8, -11, -11, -10, -10, -10, -10, -10, -10, -10, -10, -11, -11, -8, -3, 3, 8, 11, 11, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
+	static data_t buff[N] = {};
+
+	//Shift and add new input value x
+	*y = 0;
+
+	TDL:
+	for(int k = N-1; k > 0; k--)
+	{
+		#pragma HLS array_partition variable = buff complete
+		#pragma HLS unroll
+		#pragma HLS pipeline II = 1
+		//Push the buffer down.
+		buff[k] = buff[k-1];
+
+	}
+
+	//Update buff with latest data
+	buff[0] = x;
+
+	MAC:
+	for(int j = N-1; j > 0; j--)
+	{
+		#pragma HLS array_partition variable = c complete
+		#pragma HLS unroll
+		#pragma HLS pipeline II = 1
+		//Multiple and accumulate.
+		*y = *y + (buff[j]*c[j]);
+	}
+
+
+
+	*y += buff[0] * c[0];
+
+}
+
+
